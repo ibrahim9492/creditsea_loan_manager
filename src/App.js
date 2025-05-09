2f146e85968bd61ab89081707e4c5ae36b88c1f4
ibrahim9492 <124784438+ibrahim9492@users.noreply.github.com>
2025-05-09 11:43:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import AdminDashboard from "./pages/AdminDashboard";
+import Login from "./pages/Login";
+import UserDashboard from "./pages/UserDashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/user" element={<UserDashboard />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+        
+    </div>
+  );
+}
+
+export default App;
